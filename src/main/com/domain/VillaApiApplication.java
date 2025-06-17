bb38de3e1f3071615e5ac4866f8fbf93bcdbb2a3
pramisaradiah <diahpramisara@gmail.com>
2025-06-17 11:26:07 +0800

    Revisi kode pada VillaApiApplication.java

---
+++ src/main/com/domain/VillaApiApplication.java
@@ -0,0 +1,14 @@
+package main.com.domain;
+
+import main.com.domain.Server.Server;
+
+public class VillaApiApplication {
+     public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        System.out.printf("Listening on port: %s...\n", port);
+        new Server(port);
+    }
+}
