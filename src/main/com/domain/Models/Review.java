abd923d83453710f020857573e87984dc674521b
pramisaradiah <diahpramisara@gmail.com>
2025-06-24 12:54:04 +0800

    Menambahkan model Review.java

---
+++ src/main/com/domain/Models/Review.java
@@ -0,0 +1,40 @@
+package main.com.domain.Models;
+
+public class Review {
+    private int booking;
+    private int star;
+    private String title;
+    private String content;
+
+    public int getBooking() {
+        return booking;
+    }
+
+    public void setBooking(int booking) {
+        this.booking = booking;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
